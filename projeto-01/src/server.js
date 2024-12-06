99a14450d1b989cc2b47ef20c7e48d145ad6aaae
Ícaro Cazé Nunes <65853536+icarocnunes@users.noreply.github.com>
2024-12-06 01:27:31 +0000

    atualização do código principal

--- projeto-01/src/server.js
+++ projeto-01/src/server.js
@@ -1,7 +1,20 @@
 import http from "node:http";
+import { jsonHanddler } from "./middlewares/jsonHanddler.js";
 
-const server = http.createServer((req, res) => {
-  return res.end("Resposta 2");
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
+
+  await jsonHanddler(req, res);
+
+  if (method === "GET" && url === "/products") {
+    return res.end("Lista de produtos");
+  }
+
+  if (method === "POST" && url === "/products") {
+    return res.end(JSON.stringify(req.body));
+  }
+
+  return res.writeHead(404).end("Rota não encontrada");
 });
 
 server.listen(3333);
